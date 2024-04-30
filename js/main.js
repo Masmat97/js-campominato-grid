29ace1d5642caaa96b7673f97f683248e5c71fab
Matteo Mascio <matteo.mascio@libero.it>
2024-04-30 16:20:09 +0200

    M3

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ let griglia = document.getElementById("grid");
 
 for(let i = 1; i <= 100; i++){
     
-    let square= document.createElement("div");
+    let square = document.createElement("div");
     square.classList.add("square");
     square.innerText = i;
+    
+
+    square.addEventListener("click", function(){
+        console.log();
+    });
+
     griglia.append(square);
-
-
-
 }
+
