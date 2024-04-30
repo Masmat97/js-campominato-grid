e7c6e199e9a62e6837ac4c47c97887137c761b94
Matteo Mascio <matteo.mascio@libero.it>
2024-04-30 17:12:53 +0200

    M4

--- js/main.js
+++ js/main.js
@@ -2,15 +2,27 @@ let griglia = document.getElementById("grid");
 
 for(let i = 1; i <= 100; i++){
     
+    // let square = document.createElement("div");
+    // square.classList.add("square");
+    // square.innerText = i;
+    let elemento = creaQuadrato(i);
+    griglia.append(elemento);
+}
+
+function creaQuadrato(contenuto){
     let square = document.createElement("div");
-    square.classList.add("square");
-    square.innerText = i;
-    
+	square.classList.add("square");
+    square.innerText = contenuto;
+
 
     square.addEventListener("click", function(){
-        console.log();
+        console.log(this);
+        this.classList.add("cambio")
     });
 
-    griglia.append(square);
+    return square
+
 }
+
+
 
