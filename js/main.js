66d71996133b9bc667e2394a0def9f36488c1b85
Matteo Mascio <matteo.mascio@libero.it>
2024-05-02 10:51:51 +0200

    M3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let griglia = document.getElementById("grid");
 
-for(let i = 1; i <= 100; i++){
-    
+for (let i = 1; i <= 100; i++) {
+
     // let square = document.createElement("div");
     // square.classList.add("square");
     // square.innerText = i;
@@ -9,15 +9,26 @@ for(let i = 1; i <= 100; i++){
     griglia.append(elemento);
 }
 
-function creaQuadrato(contenuto){
+function creaQuadrato(contenuto) {
     let square = document.createElement("div");
-	square.classList.add("square");
+    square.classList.add("square");
     square.innerText = contenuto;
 
 
-    square.addEventListener("click", function(){
+    square.addEventListener("click", function () {
         console.log(this);
-        this.classList.add("cambio")
+
+
+        if(this.classList.contains("cambio")){
+            this.classList.remove("cambio");
+        }else {
+            this.classList.add("cambio");
+
+        }
+           
+        
+            
+
     });
 
     return square
